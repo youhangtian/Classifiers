d53caa611d232e868d85e409b692fc84db33dcaf
youhangtian <1013515028@qq.com>
2016-01-28 19:40:42 +0800

    Update kMeans.py

--- kMeans.py
+++ kMeans.py
@@ -1,54 +1,61 @@
 import numpy as np
 
-def kMeans(cs, d, steps = 10):
+def kMeans(data, k, steps = 10):
+    data = np.array(data)
+    size = len(data)
+    dimension = len(data[0])
+    
+    mins = []
+    maxs = []
+    for i in xrange(dimension):
+        mins.append(min(data[:,i]))
+        maxs.append(max(data[:,i]))
+    
     centers = []
-    for c in cs:
+    for i in xrange(k):
+        c = []
+        for j in xrange(dimension):
+            c.append(mins[j] + (maxs[j] - mins[j]) * np.random.random())
         centers.append(c)
-    data = np.array(d)
+        
+    clusters = []
     
     for step in xrange(steps):
-        centerSize = len(centers)
-        dimension = len(data[0])
-
         dist = []
-        for i in xrange(centerSize):
+        for i in xrange(size):
             dist.append([])
 
-        for d in data:
-            for i in xrange(centerSize):
-                num = 0.0
-                for j in xrange(dimension):
-                    num = num + pow(d[j] - centers[i][j], 2)
-                dist[i].append(num)
+        for i in xrange(size):
+            for j in xrange(k):
+                d = data[i] - centers[j]
+                dist[i].append(np.dot(d, d))
 
-        data2 = []
-        for i in xrange(centerSize):
-            data2.append([])
+        clusters = []
+        for i in xrange(k):
+            clusters.append([])
 
-        for i in xrange(len(data)):
+        for i in xrange(size):
             arr = []
-            for j in xrange(centerSize):
-                arr.append((dist[j][i], j))
+            for j in xrange(k):
+                arr.append((dist[i][j], j))
             a = min(arr)
 
-            data2[a[1]].append(data[i])
-
-        data2 = np.array(data2)
+            clusters[a[1]].append(i)
 
         centers = []
-        for i in xrange(centerSize):
-            centers.append([])
-
-        for i in xrange(centerSize):
-            data2[i] = np.array(data2[i])
-            for j in xrange(dimension):
-                centers[i].append(np.mean(data2[i][:,j]))
+        for i in xrange(k):
+            if (len(clusters[i]) == 0):
+                centers.append(np.random.random(dimension))
+            else:
+                c = []
+                for j in xrange(dimension):
+                    c.append(np.mean(data[clusters[i], j]))
+                centers.append(c)
                 
-    return centers
+    return centers, clusters
 
 data = [[1.0, 1.0], [1.5, 2.0], [3.0, 4.0], [5.0, 7.0], [3.5, 5.0], [4.5, 5.0], [3.5, 4.5]]
-centers = [(1.8, 2.3), (4.1, 5.4)]
+centers, clusters = kMeans(data, 2)
 
-centers = kMeans(centers, data)
-for c in centers:
-    print c
+print centers
+print clusters
